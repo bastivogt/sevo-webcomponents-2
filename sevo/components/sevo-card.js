78f70968683d78ee5eed8f45013daba28367adfb
Sevo <betont.online@googlemail.com>
2023-11-27 10:28:32 +0100

    add aspect-ratio to card

--- sevo/components/sevo-card.js
+++ sevo/components/sevo-card.js
@@ -31,6 +31,7 @@ class SevoCard extends HTMLElement {
             --box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.4);
             --margin: 20px;
             --border-radius: 4px;
+            --aspect-ratio: 4 / 3;
 
         }
 
@@ -53,6 +54,7 @@ class SevoCard extends HTMLElement {
         #card-content {
             padding: var(--section-padding);
         }
+
 
         #card-footer {
             padding: var(--section-padding);
@@ -77,7 +79,11 @@ class SevoCard extends HTMLElement {
             height: auto;
             border-top-left-radius: var(--border-radius);
             border-top-right-radius: var(--border-radius);
+            aspect-ratio: var(--aspect-ratio);
+            object-fit: cover;
         }
+
+
     `;
   }
 
@@ -125,6 +131,53 @@ class SevoCard extends HTMLElement {
   disconnectedCallback() {}
 
   // Properties
+  // background-color
+  get backgroundColor() {
+    return this.getAttribute("background-color");
+  }
+  set backgroundColor(value) {
+    this.setAttribute("background-color", value);
+  }
+
+  // section-padding
+  get sectionPadding() {
+    return this.getAttribute("section-padding");
+  }
+  set sectionPadding(value) {
+    this.setAttribute("section-padding", value);
+  }
+
+  // border-color
+  get borderColor() {
+    return this.getAttribute("border-color");
+  }
+  set borderColor(value) {
+    this.setAttribute("border-color", value);
+  }
+
+  // box-shadow
+  get boxShadow() {
+    return this.getAttribute("box-shadow");
+  }
+  set boxShadow(value) {
+    this.setAttribute("box-shadow", value);
+  }
+
+  // border-radius
+  get borderRadius() {
+    return this.getAttribute("border-radius");
+  }
+  set borderRadius(value) {
+    this.setAttribute("border-radius", value);
+  }
+
+  // aspect-ratio
+  get aspectRatio() {
+    return this.getAttribute("aspect-ratio");
+  }
+  set aspectRatio(value) {
+    this.setAttribute("aspect-ratio", value);
+  }
 
   // CSS vars
   _getCssVar(name) {
@@ -148,6 +201,36 @@ class SevoCard extends HTMLElement {
 
     if (this._elements.slotFooter.assignedElements().length === 0) {
       this._elements.footer.remove();
+    }
+
+    // background-color
+    if (this.backgroundColor) {
+      this._setCssVar("--background-color", this.backgroundColor);
+    }
+
+    // section-padding
+    if (this.sectionPadding) {
+      this._setCssVar("--section-padding", this.sectionPadding);
+    }
+
+    // border-color
+    if (this.borderColor) {
+      this._setCssVar("--border-color", this.borderColor);
+    }
+
+    // box-shadow
+    if (this.boxShadow) {
+      this._setCssVar("--box-shadow", this.boxShadow);
+    }
+
+    // border-radius
+    if (this.borderRadius) {
+      this._setCssVar("--border-radius", this.borderRadius);
+    }
+
+    // aspect-ratio
+    if (this.aspectRatio) {
+      this._setCssVar("--aspect-ratio", this.aspectRatio);
     }
   }
 }
